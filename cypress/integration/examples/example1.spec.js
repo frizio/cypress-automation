9d9cdd59932ecc6d98feb50ad2927ca8ff77733e
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2020-07-14 16:41:48 +0200

    Run a basic test case with test runner

---
+++ cypress/integration/examples/example1.spec.js
@@ -0,0 +1,11 @@
+describe('My first Test', () => {
+
+  it('Does not do much!', () => {
+    expect(true).to.equal(true)
+  }),
+  
+  it('My first Test case', () => {
+    cy.visit("https://github.com/frizio/");
+  })
+
+})

65dabb8a362bcf55833713f5b73741ca8107d582
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2020-07-15 09:57:18 +0200

    Parent - Child chaining

--- cypress/integration/examples/example1.spec.js
+++ cypress/integration/examples/example1.spec.js
@@ -11,6 +11,12 @@ describe('My first Test', () => {
     cy.get('.search-keyword').type('ca');
     cy.wait(2000);
     cy.get('.product:visible').should('have.length', 4);
+    // Parent - Child chaining
+    cy.get('.products').find('.product').should('have.length', 4);
+    
+    cy.get('.products').find('.product').eq(2).contains('ADD TO CART').click();
+    //cy.get(':nth-child(3) > .product-action > button').click();
+
   })
 
 })

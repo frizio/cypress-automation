fcc1a050501e0a57c83983c9f734e838e5c891d8
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2020-07-20 11:18:00 +0200

    commit

--- cypress/integration/examples/example2.spec.js
+++ cypress/integration/examples/example2.spec.js
@@ -2,16 +2,15 @@
 
 describe('My Second Test Suite', () => {
   
-  it('My Second Test case', () => {
+  it('My Second Test Case', () => {
 
     cy.visit("https://rahulshettyacademy.com/AutomationPractice/");
 
+    // Checkboxes
     cy.get('#checkBoxOption1').check().should('be.checked').and('have.value', 'option1');
-
     cy.get('#checkBoxOption1').uncheck().should('not.be.checked');
-
     cy.get('input[type="checkbox"]').check(['option2', 'option3']);
 
-  })
+  });
 
 })

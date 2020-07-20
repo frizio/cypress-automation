5e7dc1b0001dd3378ccce9d7c2e97b7d08d2da8a
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2020-07-20 11:44:02 +0200

    Handling Dynamic dropdowns with each command iteration

--- cypress/integration/examples/example2.spec.js
+++ cypress/integration/examples/example2.spec.js
@@ -12,7 +12,16 @@ describe('My Second Test Suite', () => {
     cy.get('input[type="checkbox"]').check(['option2', 'option3']);
 
     // Static Dropdown
-    cy.get('select').select('option2').should('have.value', 'option3');
+    cy.get('select').select('option2').should('have.value', 'option2');
+
+    // Dynamic Dropdown (Autocomplete)
+    cy.get('#autocomplete').type('ind');
+    cy.get('.ui-menu-item div').each( ($el, index, $list) => {
+      if ($el.text() === 'India') {
+        $el.click();
+      }  
+    });
+    cy.get('#autocomplete').should('have.value', 'India');
 
   });
 

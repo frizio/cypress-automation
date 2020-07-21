c469640b5530a3f318be254b25ca682400499926
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2020-07-21 12:25:14 +0200

    Handling Alert and Popup

---
+++ cypress/integration/examples/example3.spec.js
@@ -0,0 +1,23 @@
+/// <reference types="cypress" />
+
+describe('My Third Test Suite', () => {
+  
+  it('My Third Test Case', () => {
+
+    cy.visit("https://rahulshettyacademy.com/AutomationPractice/");
+    cy.get('#alertbtn').click();
+    cy.get('[value="Confirm"]').click();
+
+    // Alert and Popup: Auto accept them by default
+    // Manually firing and managing browser events
+    cy.on('window:alert', (msg) => {
+      expect(msg).to.equal('Hello , share this practice page and share your knowledge');
+    })
+
+    cy.on('window:confirm', (msg) => {
+      expect(msg).to.equal('Hello , Are you sure you want to confirm?');
+    })
+
+  });
+
+})

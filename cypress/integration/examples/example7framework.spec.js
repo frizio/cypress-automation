e0e7830e8eeb5726de359907cfbd69e7089e427c
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2020-08-28 12:16:55 +0200

    NOTEE: Use function() callback to use this context

--- cypress/integration/examples/example7framework.spec.js
+++ cypress/integration/examples/example7framework.spec.js
@@ -4,17 +4,17 @@ describe('The Test Suite', () => {
 
   before( () => {
     // Setup
-
-
+    cy.fixture('example.json').then( function(data) {
+       
+    })
   })
   
-  it('The 1st Test Case', () => {
+  it('The 1st Test Case', function() {
 
     cy.visit("https://rahulshettyacademy.com/angularpractice/")
     cy.get("input[name='name']:nth-child(2)").type("Bob")
     cy.get('select').select("Female")
 
   });
-
 
 })

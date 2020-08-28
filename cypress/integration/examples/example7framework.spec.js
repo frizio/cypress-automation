f4797f622e547ec990271d0751b62542f6886c56
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2020-08-28 12:33:14 +0200

    Validation: three examples

--- cypress/integration/examples/example7framework.spec.js
+++ cypress/integration/examples/example7framework.spec.js
@@ -14,6 +14,13 @@ describe('The Test Suite', () => {
     cy.visit("https://rahulshettyacademy.com/angularpractice/")
     cy.get("input[name='name']:nth-child(2)").type(this.data.name)
     cy.get('select').select(this.data.gender)
+    
+    // Validation 1 : Two way data binding
+    cy.get(':nth-child(4) > .ng-untouched').should('have.value', this.data.name)
+    // Validation 2 : Min length of a text field
+    cy.get("input[name='name']:nth-child(2)").should('have.attr', 'minlength', '2')
+    // Validation 3 : Disabled field
+    cy.get('#inlineRadio3').should('be.disabled')
 
   });
 

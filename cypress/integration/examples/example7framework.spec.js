f1fcaff358fb81964cd05fd287b6ff4c27c66048
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2020-08-28 12:19:41 +0200

    Use fixture to load data

--- cypress/integration/examples/example7framework.spec.js
+++ cypress/integration/examples/example7framework.spec.js
@@ -5,15 +5,15 @@ describe('The Test Suite', () => {
   before( () => {
     // Setup
     cy.fixture('example.json').then( function(data) {
-       
+       this.data = data;
     })
   })
   
   it('The 1st Test Case', function() {
 
     cy.visit("https://rahulshettyacademy.com/angularpractice/")
-    cy.get("input[name='name']:nth-child(2)").type("Bob")
-    cy.get('select').select("Female")
+    cy.get("input[name='name']:nth-child(2)").type(this.data.name)
+    cy.get('select').select(this.data.gender)
 
   });
 

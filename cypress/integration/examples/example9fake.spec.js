a806f40a7982e475b692be4ec787f9996a7c48f1
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2020-09-04 16:22:52 +0200

    Mokcing API with stub response

---
+++ cypress/integration/examples/example9fake.spec.js
@@ -0,0 +1,31 @@
+/// <reference types="cypress" />
+import HomePage from "../../support/pageObjects/HomePage"
+import ShopPage from "../../support/pageObjects/ShopPage"
+
+describe('Another xhr Test Suite', () => {
+  
+  it('The 1st Test Case', function() {
+
+    cy.visit("https://example.cypress.io/commands/network-requests")
+    cy.server()
+
+    cy.route( {
+      method: 'PUT',
+      url: "comments/*",
+      status: 404,
+      response: {
+        error: "Comment not exist!"
+      },
+      deplay: 1000
+    } ).as('UpdateComment')
+
+    cy.get('.network-put').click()
+
+    cy.get('.network-put-comment').should('contain', 'Comment not exist!')
+
+  });
+
+})
+
+// Example website
+// https://example.cypress.io/commands/network-requests

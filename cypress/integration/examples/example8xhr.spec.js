a648b56ffaef37a24c0b09db77b14bfd5b41999d
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2020-09-04 15:55:55 +0200

    Manipulating POST network request call

--- cypress/integration/examples/example8xhr.spec.js
+++ cypress/integration/examples/example8xhr.spec.js
@@ -17,6 +17,24 @@ describe('The xhr Test Suite', () => {
 
   });
 
+  it('POST request Test Case', function() {
+
+    // Listen to POST to comments
+    cy.route('POST', '/comments').as('postComment')
+
+    // we have code that posts a comment when the button is clicked in scripts.js
+    cy.get('.network-post').click()
+    cy.wait('@postComment')
+
+    // get the route
+    cy.get('@postComment').should((xhr) => {
+        expect(xhr.requestBody).to.include('email')
+        expect(xhr.requestHeaders).to.have.property('Content-Type')
+        expect(xhr.responseBody).to.have.property('name', 'Using POST in cy.route()')
+    })
+
+  });
+
 })
 
 // Example website
